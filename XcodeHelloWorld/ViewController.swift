6c39a16d6a40b2b58334d5787194555aa89dfdab
Chris Sidi <chrisid@microsoft.com>
2018-09-05 19:29:46 -0400

    Initial Commit

---
+++ XcodeHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeHelloWorld
+//
+//  Created by Chris Sidi on 9/5/18.
+//  Copyright © 2018 Chris Sidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
